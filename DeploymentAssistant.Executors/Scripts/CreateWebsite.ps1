95b6c525f6a92268c0ad3d3890232d21952bedae
boopalan.j <boopalan.j@payoda.com>
2019-10-21 23:26:13 +0530

    CreateWebsite script file added

---
+++ DeploymentAssistant.Executors/Scripts/CreateWebsite.ps1
@@ -0,0 +1,7 @@
+function CreateWebsite()
+{
+    param([String]$websiteName, [System.Collections.Generic.Dictionary[string, string]]$bindings, [String]$physicalPath, [Boolean]$override)
+
+    
+    
+}
